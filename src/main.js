4de5077cd43d7b6ee689a38a6573d910a9479b13
Big-Ding <dingteng27@126.com>
2020-08-01 21:18:49 +0800

    提交reset.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import "reset-css";
 import App from "./App.vue";
 import "./assets/css/icon.css";
 import GButton from "./components/button.vue";
 import ButtonGroup from "./components/ButtonGroup.vue";
 import "../src/assets/js/icon";
+import "./assets/css/reset.css";
 Vue.config.productionTip = false;
 
 Vue.component("g-button", GButton);
